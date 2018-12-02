1e6f215491c022cf6cdce153aa3c30af24dc7e11
sureshvairamuthu <sureshvairamuthu@gmail.com>
2018-12-01 17:48:56 -0800

    meteor js tutorial to application

--- todo_application/tests/main.js
+++ todo_application/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("simple-todos", function () {
+describe("todos", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "simple-todos");
+    assert.strictEqual(name, "todos");
   });
 
   if (Meteor.isClient) {
